af2e4160b4ddfdb97e85e184b4c024a8c1060708
Ledze91 <dd199191@abv.bg>
2016-01-16 10:00:57 +0200

    Homework
    Homework

---
+++ Code-Formatting-Fixed-Homework/Student.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+
+public class Student
+{
+
+    public string 
+        Name { get;
+                     set; }
+    public
+        int Age { get
+            ; set; }
+        public List
+        <double>    Grades {     get;
+        set; }
+
+    public  Student()
+    {
+        this.Grades 
+            = new 
+            List<double>
+            ();
+    }
+
+    public override             bool Equals
+        (object param)
+    {
+
+        Student 
+            student = param as 
+            Student;
+
+
+        if (student == null)
+        {
+            return false;
+        }
+
+        
+        if (!Object.Equals(this.Name, student.Name))
+
+
+                return false;
+        
+
+       
+        if (this.Age !=
+            student.Age)
+
+
+            return false;
+        
+
+        return true;
+    }
+
+    public static bool 
+        operator ==
+        (Student student1
+        , Student student2)
+    {
+        return Student.
+            Equals(student1
+            , student2)
+            ;
+    }
+
+
+    public 
+        static bool 
+        operator !=(Student student1
+        , Student student2)
+
+    {
+        return !(Student.
+            Equals(student1, student2)
+            );
+    }
+
+    public override int GetHashCode(
+        )
+    {
+
+        return this.
+            Name.GetHashCode(
+            ) ^ this.Age
+            .GetHashCode();
+    }
+
+    public 
+        override string 
+        ToString()
+    {
+        return
+            String.
+            Format
+                                (
+                            "Student(Name: {0}" +
+                     ", Age: {1})",
+                  this.Name ?? "" +
+              "<unnamed>", this.
+             Age);
+    }
+
+    public  
+        int             CompareTo
+                                    (Student other)
+    {
+
+                if (other 
+            == null
+            )
+        
+
+            throw new
+                ArgumentException("Student " +
+                                  "is null");
+        
+
+        int ageComparison 
+            = this.Age.CompareTo
+            (other.Age);
+        if (ageComparison 
+            == 0)
+        
+            return this
+                .Name.CompareTo(other.Name);
+        
+
+        return ageComparison;
+    }
+
+         public static bool 
+         operator <(Student a,
+                Student b)
+    {
+        return 
+            a.CompareTo(
+                b) <
+                0;
+    }
+
+    public static
+        bool operator >
+        (Student a, Student b)
+    {
+        return a
+            .CompareTo(b) > 0;
+    }
+}
